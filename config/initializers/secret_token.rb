98ff7aefec796c48c5dd315901b890de7e315689
Colin MacKenzie <colinwmackenzie@gmail.com>
2014-01-16 18:29:47 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myathalon::Application.config.secret_token = 'e823de45b99f43b796a399103d34a53f51d5319aefa32580c7caffa6c69d1691fde9c5c849e313b2d5f4c245fcce1e107ef8de148808aea0f8282f3ab84593c6'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Myathalon::Application.routes.draw do
+  devise_for :users
+
+  root to: "users#show"
+end
