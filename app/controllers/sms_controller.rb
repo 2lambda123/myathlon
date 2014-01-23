9a75d4c6107a58329f1d69153ad5dcfd6cc08f2d
Colin MacKenzie <colinwmackenzie@gmail.com>
2014-01-22 22:50:25 -0500

    some more controller stuff

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,9 +1,18 @@
 class SmsController < ApplicationController
   def receive
+    @client = Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
+    
     sender = params[:From]
     @twiml = Twilio::TwiML::Response.new do |r|
         r.Message "Hey Monkey. Thanks for the message! Your number is #{sender}."
     end
+    
+    @client.account.messages.create(
+      from: '+19795894945',
+      to: params[:From],
+      body: 'Oh hello!'
+    )
+    
     @twiml.text
   end
 end
