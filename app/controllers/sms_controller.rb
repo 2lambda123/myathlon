0710f30f202e7d9f4634263d64dadf451310b6a9
Colin MacKenzie <colinwmackenzie@gmail.com>
2014-01-22 22:39:35 -0500

    added some params manipulation

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,7 +1,8 @@
 class SmsController < ApplicationController
   def receive
+    sender = params[:From]
     @twiml = Twilio::TwiML::Response.new do |r|
-        r.Message "Hey Monkey. Thanks for the message!"
+        r.Message "Hey Monkey. Thanks for the message! Your number is #{sender}."
     end
     @twiml.text
   end
