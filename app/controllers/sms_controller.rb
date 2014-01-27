af093ec4f95f1d3d325cad23fd5f5ecb667bfd54
Colin MacKenzie <colinwmackenzie@gmail.com>
2014-01-26 19:36:50 -0500

    a little debugging

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -2,11 +2,11 @@ class SmsController < ApplicationController
   def receive
     @client = Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
     process_text(params[:From], params[:Body])
-    # 
-    # @client.account.messages.create(
-    #   from: '+19795894945',
-    #   to: params[:From],
-    #   body: 'Oh hello!'
-    # )    
+    
+    @client.account.messages.create(
+      from: '+19795894945',
+      to: params[:From],
+      body: "Message received - #{params[:Body]}
+    )    
   end
 end
